e3a59e18ba4c368c91a42b6160f7c5294ba59df2
root <desalegnbekele2121@gmail.com>
2021-08-31 09:28:42 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; c++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
