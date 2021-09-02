83123f26f76271f62adfeafe23ef9a4789657bcb
noeldesalegn <88342716+noeldesalegn@users.noreply.github.com>
2021-09-02 18:09:18 +0300

    Create positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"                                                  
+/**                                                                
+ * main - tests function that prints if integer is positive or neg\
+ive                                                                
+ * Return: 0                                                       
+ */                                                                
+int main(void)                                                     
+{                                                                  
+int i;                                                             
+if (n > 0)                                                         
+{                                                                  
+printf("%d is positive\n", n);                                     
+}                                                                  
+else if (n == 0)                                                   
+{                                                                  
+printf("%d is zero\n", n);                                         
+}                                                                  
+else                                                               
+{                                                                  
+printf("%d is negative\n", n);                                     
+}                                                                  
+return (0);                                                        
+}                                                                  
+ 
