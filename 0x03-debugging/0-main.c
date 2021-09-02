52f8fdedd14f79fcea673cabc087d57304c35c8f
root <desalegnbekele2121@gmail.com>
2021-09-02 11:30:55 -0700

    fun

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,26 +3,10 @@
  * main - tests function that prints if integer is positive or negive
  * Return: 0
  */
-positive_or_negative(int n)
-{
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
-return (i); 
-}
 int main(void)
 {
 int i;
-i = 98;
+i=98;
 positive_or_negative(i);
 return (0);
 }
