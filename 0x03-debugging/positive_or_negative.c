35c21cecce88bde0e4b8b84490d73b41de437fad
root <desalegnbekele2121@gmail.com>
2021-09-02 10:44:35 -0700

    comit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negive
@@ -6,7 +5,7 @@
  */
 int main(void)
 {
-int i;
+int n;
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -21,29 +20,3 @@ printf("%d is negative\n", n);
 }
 return (0);
 }
-=======
-#include "main.h"                                                  
-/**                                                                
- * main - tests function that prints if integer is positive or neg\
-ive                                                                
- * Return: 0                                                       
- */                                                                
-int main(void)                                                     
-{                                                                  
-int i;                                                             
-if (n > 0)                                                         
-{                                                                  
-printf("%d is positive\n", n);                                     
-}                                                                  
-else if (n == 0)                                                   
-{                                                                  
-printf("%d is zero\n", n);                                         
-}                                                                  
-else                                                               
-{                                                                  
-printf("%d is negative\n", n);                                     
-}                                                                  
-return (0);                                                        
-}                                                                  
- 
->>>>>>> 83123f26f76271f62adfeafe23ef9a4789657bcb
