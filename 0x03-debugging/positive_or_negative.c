fb7fd36c32546499b055fb959b8d260ad6356f23
root <desalegnbekele2121@gmail.com>
2021-09-02 08:11:45 -0700

    git

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negive
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
