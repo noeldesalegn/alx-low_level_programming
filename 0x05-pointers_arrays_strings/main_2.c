0d56f40cb3ed96e48b50b7d2f3295843501788f9
root <desalegnbekele2121@gmail.com>
2021-09-06 11:15:55 -0700

    A good engineer thinks in reverse and asks himself

---
+++ 0x05-pointers_arrays_strings/main_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void rev_string(char *);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char s[1] = "";
+printf("%s\n", s);
+rev_string(s);
+printf("%s\n", s);
+return (0);  
+}
