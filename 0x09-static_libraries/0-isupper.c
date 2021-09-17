d9116d75f507462e07c28cf7b8e0f07c51e64a3e
noeldesalegn <88342716+noeldesalegn@users.noreply.github.com>
2021-09-17 23:40:45 +0300

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,49 +1,22 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 /**
-
  * _isupper - checks for uppercase character
-
  *
-
  * @c: parameter hard-coded in main
-
  *
-
  * Return: 0 or 1
-
  */
-
-
-
 int _isupper(int c)
-  
 {
-  
-  int i;
-  
-
-  
-  if (c >= 'A' && c <= 'Z')
-    
-    {
-      
-      i = 1;
-      
-      return (i);
-      
-    }
-  
-  else
-    
-    {
-      
-      i = 0;
-      
-      return (i);
-      
-    }
-  
+int i;
+if (c >= 'A' && c <= 'Z')
+{
+i = 1;      
+return (i);
+}
+else
+{
+i = 0;
+return (i);
+}  
 }
