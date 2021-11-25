6d1733c00ab82a4b23bcc68eaa8ff82ce228a6fa
root <desalegnbekele2121@gmail.com>
2021-11-25 14:39:31 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: The list
@@ -9,22 +8,21 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
-	listint_t *p = h ? h->next : NULL;
-
-	if (h)
-	{
-		printf("%d\n", h->n);
-		num++;
-	}
-	if (p)
-	{
-		while (p)
-		{
-			printf("%d\n", p->n);
-			num++;
-			p = p->next;
-		}
-	}
-	return (num);
+size_t num = 0;
+listint_t *p = h ? h->next : NULL;
+if (h)
+{
+printf("%d\n", h->n);
+num++;
+}
+if (p)
+{
+while (p)
+{
+printf("%d\n", p->n);
+num++;
+p = p->next;
+}
+}
+return (num);
 }
